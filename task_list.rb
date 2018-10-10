015f04b63ba482f7cdcf804c26c9a1911a88f211
Henrique <hpmoreira2009@hotmail.com>
2018-10-10 15:40:09 -0300

    Exercício task_list

--- task_list.rb
+++ task_list.rb
@@ -1,3 +1,5 @@
+opcao = ""
+while opcao != 3 do
 puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
 puts '[1] Inserir uma tarefa'
 puts '[2] Ver todas as tarefas'
@@ -6,19 +8,22 @@ puts
 print 'Opção escolhida: '
 
 opcao = gets.to_i
-tarefa = ""
+tarefa = "" 
 
-while opcao != 3 do
+
   if opcao == 1
     print 'Digite sua tarefa: '
     tarefa = gets.chomp()
     puts
     puts 'Tarefa cadastrada: ' + tarefa
+    puts
   elsif opcao == 2
     puts
     puts tarefa
+    puts
   else
     puts
     puts 'Opção inválida'
+    puts
   end
 end
