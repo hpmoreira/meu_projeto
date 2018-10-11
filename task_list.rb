56c1f7b30bb1b3c1a255f0a493fd8267606daf0a
Henrique <hpmoreira2009@hotmail.com>
2018-10-11 14:31:31 -0300

    Exercício método menu

--- task_list.rb
+++ task_list.rb
@@ -1,29 +1,38 @@
-opcao = ""
-while opcao != 3 do
+def menu
+  puts "Escolha uma opção no menu: \n"
+  puts '[1] Inserir uma tarefa'
+  puts '[2] Ver todas as tarefas'
+  puts '[3] Sair'
+  puts
+  print 'Opção escolhida: '
+end
+
 puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
-puts '[1] Inserir uma tarefa'
-puts '[2] Ver todas as tarefas'
-puts '[3] Sair'
-puts
-print 'Opção escolhida: '
-
+puts menu
 opcao = gets.to_i
-tarefa = "" 
+tarefa = ""
 
-
+while (opcao != 3) do
   if opcao == 1
     print 'Digite sua tarefa: '
-    tarefa = gets.chomp()
+    tarefa = gets.chomp
     puts
     puts 'Tarefa cadastrada: ' + tarefa
     puts
-  elsif opcao == 2
+    opcao = menu
+    opcao = gets.to_i
+    elsif opcao == 2
     puts
     puts tarefa
     puts
+    opcao = menu
+    opcao = gets.to_i
   else
     puts
     puts 'Opção inválida'
     puts
+    opcao = menu
+    opcao = gets.to_i
   end
 end
+
